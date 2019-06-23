bfe8e9efffc949c51dbdb3f47a32dfa122c2533d
Anshuman Singh <anshumansingh005@gmail.com>
2019-06-23 23:21:43 +0530

    Added JS part for header and home page

---
+++ stub/js/header.js
@@ -0,0 +1,15 @@
+function openSignIn() {
+    document.getElementById('signInModal').style.display = 'block';
+}
+
+function openSignUp() {
+    document.getElementById('signUpModal').style.display = 'block';
+}
+
+function onClickModalClicked() {
+    document.getElementById('signInModal').style.display = 'none';
+}
+
+function openSignUpButton() {
+    document.getElementById('signUpModal').style.display = 'block'
+}
